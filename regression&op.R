43e04f05b5e0783ba37396b3469a4526fa13df4f
JTang131 <32047362+JTang131@users.noreply.github.com>
2020-11-30 23:10:54 -0500

    Add files via upload

--- regression&op.R
+++ regression&op.R
@@ -4,25 +4,49 @@ library(lmtest)
 library(plm)
 library(ggplot2)
 library(pracma)
-
+library(stargazer)
+library(plm)
 
 dat1 <- read.csv("E:/firm project/data/dat1.csv", header=FALSE)
 
 attach(dat1)
 
 indyear=dat1$V3*10000+dat1$V4
-reg1=felm(V1~V5+I(V5*V6)|indyear+V2|0|V3,data = dat1)
+reg1=felm(V1~V5+I(V5*V6)|indyear|0|V2,data = dat1)
 summary(reg1)
 
 dat2 <- read.csv("E:/firm project/data/dat2.csv", header=FALSE)
 attach(dat2)
 indyear=dat2$V3*10000+dat2$V4
-reg2=felm(V1~V5+V6+I(V5*V6)+V7|indyear|0|V3,data = dat2)
-summary(reg2)
+reg21=felm(V1~V5+V6+I(V5*V6)+V8+V10|indyear|0|V3,data = dat2)
+summary(reg21)
+reg22=felm(V1~V5+V7+I(V5*V7)+V9+V10|indyear|0|V3,data = dat2)
+summary(reg22)
+reg23=felm(V1~V5+V6+I(V5*V6)+V8+V10|V3+V4|0|V3,data = dat2)
+summary(reg23)
+reg24=felm(V1~V5+V7+I(V5*V7)+V9|V2+V4|0|V2,data = dat2)
+summary(reg24)
+
+#industry level regression
+#disp change
+idat1 <- read.csv("E:/firm project/agg data/idat1.csv", header=FALSE)
+attach(idat1)
+colnames(idat1)=c("DDisp","id","year","Dkappa","Emp","DTFP")
+reg25=felm(DDisp~V4+V6+V5|V2+V3|0|V2,idat1)
+summary(reg25)
+#disp level
+idat2 <- read.csv("E:/firm project/agg data/idat2.csv", header=FALSE)
+attach(idat2)
+colnames(idat2)=c("Disp","id","year","kappa","DTFP","Emp")
+reg26=felm(Disp~V4+V5+V6|V2+V3|0|V2,idat2)
+summary(reg26)
+
+stargazer(reg25,reg26)
+
 
 #solow regression
 
-op <- read.csv("E:/firm project/data/op.csv", header=FALSE)
+op <- read.csv("H:/PhD/firm project/data/op.csv", header=FALSE)
 attach(op)
 #fixed effect
 reg3=felm(log(V4)~log(V5)+log(V6)|V1+V2|0|V3,data = op)
